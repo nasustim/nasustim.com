7dea0c8d53368db80f8cd752774ed5963b0c0418
nasustim <mitsuhibino@gmail.com>
2020-12-13 21:40:07 +0900

    #90 GAを有効に

--- next.config.js
+++ next.config.js
@@ -4,4 +4,7 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  env: {
+    GA_TRACKING_ID_FOR_BROWSER: process.env.GA_TRACK_ID,
+  },
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,6 +14,26 @@ export default class _App extends App {
       <div className={styles.layout}>
         <Head>
           <title>{pageProps.title}</title>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          {!!process.env.GA_TRACKING_ID_FOR_BROWSER && (
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID_FOR_BROWSER}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${process.env.GA_TRACKING_ID_FOR_BROWSER}', {
+                    page_path: window.location.pathname,
+                  });`,
+                }}
+              />
+            </>
+          )}
         </Head>
 
         <div className={styles.background}>

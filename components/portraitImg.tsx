976e58e137fa6488a27e616c0b4bf1a85bb1bd3a
nasustim <mitsuhibino@gmail.com>
2020-12-27 04:24:17 +0900

    #105: PortraitImgをclass componentに変更

--- components/portraitImg.tsx
+++ components/portraitImg.tsx
@@ -1,35 +1,54 @@
-// import Image from 'next/image'
+// import Image from 'next/image' <- netlify deployにまだ対応していないらしい
+import {
+  Scene,
+} from 'three'
 import styles from './styles/portraitImg.module.scss'
 
-import { useState } from 'react'
+import React from 'react'
 
 export type ProfileProps = {
   src: string
 }
 
-const PortraitImg: React.FC<ProfileProps> = ({ src }) => {
-  const [style, setStyle] = useState([
-    [50, 50],
-    [50, 50],
-    [50, 50],
-    [50, 50],
-  ])
+type State = ProfileProps & {
+  curveStyle: number[][]
+}
 
-  const IntId = setInterval(() => {
-    clearInterval(IntId)
-    const newStyle = style.map(set)
-    setStyle(newStyle)
-  }, 5000)
-
-  const _style = {
-    borderRadius: `${style[0][0]}% ${style[1][0]}% ${style[2][0]}% ${style[3][0]}%/${style[0][1]}% ${style[1][1]}% ${style[2][1]}% ${style[3][1]}%`,
+export default class PortraitImg extends React.Component<ProfileProps, State> {
+  constructor (props) {
+    super(props)
+    this.state = {
+      curveStyle: [
+        [50, 50],
+        [50, 50],
+        [50, 50],
+        [50, 50],
+      ],
+      src: props.src
+    }
   }
-  return (
-    <div style={_style} className={styles.container}>
-      {/*<Image width={100} height={100} layout={`responsive`} src={src} />*/}
-      <img src={src} className={styles.img} />
-    </div>
-  )
+  componentDidMount () {
+    const IntId = setInterval(() => {
+      clearInterval(IntId)
+      const newStyle = this.state.curveStyle.map(set)
+      this.setState({
+        curveStyle: newStyle
+      })
+    }, 5000)
+  }
+  render () {
+    const src = this.state.src
+    const style = this.state.curveStyle
+    const _style = {
+      borderRadius: `${style[0][0]}% ${style[1][0]}% ${style[2][0]}% ${style[3][0]}%/${style[0][1]}% ${style[1][1]}% ${style[2][1]}% ${style[3][1]}%`,
+    }
+    return (
+      <div style={_style} className={styles.container}>
+        {/*<Image width={100} height={100} layout={`responsive`} src={src} />*/}
+        <img src={src} className={styles.img} />
+      </div>
+    )
+  }
 }
 
 function set() {
@@ -38,5 +57,3 @@ function set() {
 
   return [_v_0, _v_1]
 }
-
-export default PortraitImg

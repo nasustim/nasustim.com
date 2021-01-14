79cae7506b2477104bae8dcdf34b4c622f0e94f4
nasustim <mitsuhibino@gmail.com>
2021-01-15 02:37:09 +0900

    feature/add-home-logo: add header logo except top page

--- containers/common/header.tsx
+++ containers/common/header.tsx
@@ -1,10 +1,15 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 import styles from './styles/header.module.scss'
 
-const Header = () => (
-  <div className={styles.container}>
-    {/**  ナビゲーションバーとかほしい */}
-    <div className={styles.yodayoda}></div>
+const Header = () => {
+  const isTopPage: boolean = useRouter().pathname === '/'
+
+  return <div className={styles.container}>
+    <div className={styles.headerWrapper}>
+      { isTopPage ? null : <div className={styles.logo}><Link href={'/'}>nasustim.com</Link></div> }
+    </div>
   </div>
-)
+}
 
 export default Header

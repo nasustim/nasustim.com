6b3572abce13e5ee8d0730777b288935f341c242
nasustim <mitsuhibino@gmail.com>
2021-08-12 18:23:11 +0900

    update dependencies & fix legacy code

--- components/common/htmlify.tsx
+++ components/common/htmlify.tsx
@@ -1,9 +1,7 @@
-import React, { ReactChild } from 'react'
-import unified from 'unified'
+import React from 'react'
+import { unified } from 'unified'
 import parse from 'remark-parse'
-
-// tslint:disable-next-line:no-var-requires
-const remark2react = require('remark-react') // @types/remark-reactが無かったのでとりいそぎ
+import remark2react from 'remark-react'
 
 import styles from './styles/htmlify.module.scss'
 
@@ -13,7 +11,7 @@ export type Props = {
 
 const HTMLify: React.FC<Props> = ({ markdown }) => {
   // See: https://vivliostyle.github.io/vivliostyle_doc/ja/vivliostyle-user-group-vol2/spring-raining/index.html
-  const html = unified().use(parse).use(remark2react).processSync(markdown).result as ReactChild
+  const html = unified().use(parse).use(remark2react, React).processSync(markdown).result
 
   return <div className={styles.document}>{html}</div>
 }
---
+++ nasustim.com.d.ts
@@ -0,0 +1,12 @@
+type FrontMatterAttributes = {
+  title: string
+  date: string
+  description: string
+  pageid: string
+  headimg: string
+}
+
+type FrontMatter = {
+  body: string
+  attributes: FrontMatterAttributes
+}

e71cda307a33f86641d2ab7313741923f804fe67
nasustim <mitsuhibino@gmail.com>
2021-09-12 15:55:36 +0900

    update package-lock.json

--- components/common/header-link.tsx
+++ components/common/header-link.tsx
@@ -17,7 +17,8 @@ const HeaderLink: React.FC<Props> = ({ children, isCurrent, scrollTo }) => {
       onClick={(_) => {
         if (!isCurrent) scroll(scrollTo)
       }}
-      data-is-top={isCurrent ? '1' : '0'}>
+      data-is-top={isCurrent ? '1' : '0'}
+    >
       {children}
     </span>
   )

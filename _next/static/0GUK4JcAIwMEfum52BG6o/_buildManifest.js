4e96b08f07a872f7ee0942645260378d8efd50aa
nasustim <nasustim@users.noreply.github.com>
2022-01-22 18:48:03 +0000

    deploy: eaf6c02b7f268b2906adbc22e8cac6dd3cc4df1e

--- _next/static/0GUK4JcAIwMEfum52BG6o/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-daca5d5bcfabda45cae6.js"],"/404":["static/chunks/pages/404-5c0e6c09093bbc4951bf.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/404","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

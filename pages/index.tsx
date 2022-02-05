163e36e6e9537f7888884e65a280e6001336a635
nasustim <mitsuhibino@gmail.com>
2022-02-06 02:45:57 +0900

    add external web service icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,41 @@
 import React from 'react'
 
 import Image from '../components/atoms/image'
+import { SocialIcon } from 'react-social-icons'
 
 import styles from './index.module.scss'
+
+const socialServiceUrls = [
+  'https://github.com/nasustim',
+  'https://facebook.com/mitsuhibino',
+  'https://instagram.com/nasustim',
+  'https://keybase.io/nasustim',
+]
 
 type Props = SuperPageProps
 
 const Page: React.VFC<Props> = (props) => {
   return (
-    <React.Fragment>
+    <>
       <div className={styles.page_inner}>
         <h1 className='heading'>Mitsuhiro Hibino</h1>
-      </div>
-      <div className={styles.page_inner}>
         <Image src={'/images/me.jpg'} size={{ width: 100, height: 100 }} />
       </div>
       <div className={styles.page_inner}>
         <ul className='text'>
           <li>location: Tokyo</li>
           <li>job: software engineer</li>
-          <ul>
-            <li>specification: web server-side</li>
-          </ul>
-          <li>
-            links: <a href='https://facebook.com/mitsuhibino'>Facebook</a>,{' '}
-            <a href='https://facebook.com/mitsuhibino'>Github</a>
-          </li>
+          <li>specification: web server-side</li>
         </ul>
       </div>
-    </React.Fragment>
+      <div className={styles.page_inner}>
+        <div className={styles.external_profile_list}>
+          {socialServiceUrls.map((v) => (
+            <SocialIcon url={v} fgColor='transparent' />
+          ))}
+        </div>
+      </div>
+    </>
   )
 }
 

e0d85ad4a66c901db49ecb9ee4c706f8e17fc9d8
nasustim <mitsuhibino@gmail.com>
2022-02-05 23:57:11 +0900

    apply custom fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,12 @@ export default class CustomDocument extends Document {
   render() {
     return (
       <Html lang='ja'>
-        <Head />
+        <Head>
+          <link
+            href='https://fonts.googleapis.com/css2?family=Mukta:wght@600&family=Spectral:ital,wght@0,300;1,300&display=optional'
+            rel='stylesheet'
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,16 +7,17 @@ type Props = SuperPageProps
 const Page: React.VFC<Props> = (props) => {
   return (
     <React.Fragment>
-      <h1>Mitsuhiro Hibino</h1>
+      <h1 className='heading'>Mitsuhiro Hibino</h1>
       <Image src={'/images/me.jpg'} size={{ width: 100, height: 100 }} />
-      <ul>
+      <ul className='text'>
         <li>place: Tokyo</li>
         <li>job: software engineer</li>
         <ul>
           <li>specification: web server-side</li>
         </ul>
         <li>
-          link: <a href='https://twitter.com/nasustim'>Twitter</a>
+          links: <a href='https://facebook.com/mitsuhibino'>Facebook</a>,{' '}
+          <a href='https://facebook.com/mitsuhibino'>Github</a>
         </li>
       </ul>
     </React.Fragment>

06b2ac7ef12188e3d5bbe8fccd02bc6213969ad2
nasustim <mitsuhibino@gmail.com>
2022-02-08 02:01:21 +0900

    divide contents

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,26 +8,11 @@ import { faBuilding, faUser } from '@fortawesome/free-solid-svg-icons'
 
 type Props = SuperPageProps
 
-const Page: React.VFC<Props> = (props) => {
+const Page: React.VFC<Props> = () => {
   return (
     <>
       <div className={style.page_inner}>
-        <h1 className={style.title}>Mitsuhiro Hibino</h1>
-      </div>
-      <div className={style.page_inner}>
-        <div className={style.profile}>
-          <div className={style.profile_image}>
-            <TopImage src={'/images/me.jpg'} />
-          </div>
-          <div className={style.profile_text}>
-            <p>
-              <FontAwesomeIcon icon={faUser} /> Software Engineer, Creator
-            </p>
-            <p>
-              <FontAwesomeIcon icon={faBuilding} /> web service company, freelancer
-            </p>
-          </div>
-        </div>
+        <Introduction />
       </div>
     </>
   )
@@ -45,3 +30,24 @@ export const getStaticProps = async () => {
 }
 
 export default Page
+
+// Contents
+
+const Introduction: React.VFC = () => (
+  <>
+    <h1 className={style.title}>Mitsuhiro Hibino</h1>
+    <div className={style.profile}>
+      <div className={style.profile_image}>
+        <TopImage src={'/images/me.jpg'} />
+      </div>
+      <div className={style.profile_text}>
+        <p>
+          <FontAwesomeIcon icon={faUser} /> Software Engineer, Creator
+        </p>
+        <p>
+          <FontAwesomeIcon icon={faBuilding} /> web service company, freelancer
+        </p>
+      </div>
+    </div>
+  </>
+)

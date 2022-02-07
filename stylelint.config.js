5a045dafb20e1095104d8c49b5e8cb030ef3546e
nasustim <mitsuhibino@gmail.com>
2022-02-08 02:50:50 +0900

    fix profile img size

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   syntax: 'scss',
   extends: ['stylelint-config-property-sort-order-smacss'],
+  customSyntax: 'postcss-scss',
   rules: {
     'number-leading-zero': null,
   },

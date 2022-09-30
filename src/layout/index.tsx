c7d8e34216ae8e9a455619f9a70ebffc8cfa8608
nasustim <mitsuhibino@gmail.com>
2022-10-01 04:18:55 +0900

    style(layout): to responsive

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -4,7 +4,14 @@ import { css } from '@emotion/react'
 const style = css`
   width: 100%;
   height: 100%;
+`
 
+const styleInner = css`
+  width: 100%;
+  height: 100%;
+
+  margin-left: auto;
+  margin-right: auto;
   @media (min-width: 780px) {
     width: 780px;
   }
@@ -14,5 +21,9 @@ type Props = {
   children: ReactNode
 }
 
-const Layout: React.FC<Props> = ({ children }) => <div css={style}>{children}</div>
+const Layout: React.FC<Props> = ({ children }) => (
+  <div css={style}>
+    <div css={styleInner}>{children}</div>
+  </div>
+)
 export default Layout

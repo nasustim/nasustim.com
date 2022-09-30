18a089cab64e8e3094dcb6b404be9464d51293aa
nasustim <mitsuhibino@gmail.com>
2022-10-01 04:30:52 +0900

    feat(pages/index): use Text and add profile text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Layout from '../layout'
 
 import PageTitle from '../atoms/PageTitle'
 import Image from '../atoms/Image'
+import Text from '../atoms/Text'
 
 type Props = SuperPageProps
 
@@ -11,6 +12,10 @@ const Page: React.FC<Props> = (props) => {
     <Layout>
       <PageTitle text={'nasustim.com'} />
       <Image src='/static/profile_400x400.jpg' width={150} height={150} />
+      <Text>
+        岐阜県岐阜市出身、都内在住のソフトウェアエンジニア。現在は株式会社MIXIで美容サロン予約サービスのサーバサイド,
+        インフラ開発を担当している。
+      </Text>
     </Layout>
   )
 }

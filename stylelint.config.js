268c07874b663e778559ed6e1e6005386ca5d4ab
nasustim <mitsuhibino@gmail.com>
2022-12-05 00:47:14 +0900

    fix stylelint error

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   syntax: 'scss',
-  extends: ['stylelint-config-property-sort-order-smacss'],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-property-sort-order-smacss'
+  ],
   customSyntax: 'postcss-scss',
   rules: {
     'number-leading-zero': null,

846bb7868d0a649ca19b2d906d795999d526ad8a
nasustim <mitsuhibino@gmail.com>
2022-12-29 21:06:29 +0900

    feat(/): update page state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+import { useSelector, useDispatch } from 'react-redux'
+import type { RootState } from '../redux/store'
+import { actions } from '../redux/slices/page'
+
 import Index from '../components/templates/index'
 
 type Props = SuperPageProps
 
 const Page: React.FC<Props> = (props) => {
+  const prevPath = useSelector((state: RootState) => state.page.path)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    if (props.meta.canonicalPath !== prevPath) {
+      dispatch(actions.transition(props.meta.canonicalPath))
+    }
+  }, [])
+
   return <Index />
 }
 

0c895e30b86fd27139816b88d7202d6b824d4cba
nasustim <mitsuhibino@gmail.com>
2023-01-03 22:22:03 +0900

    fix(style): modify font color

--- src/components/molecules/ButtonList/index.tsx
+++ src/components/molecules/ButtonList/index.tsx
@@ -1,25 +1,12 @@
 import React, { ReactNode } from 'react'
-
-import Button from '../../atoms/Button'
 import style from './index.module.scss'
 
 interface Props {
-  contentList: Array<{
-    action: () => void
-    child: ReactNode
-  }>
+  children: ReactNode
 }
 
-const ButtonList: React.FC<Props> = (props) => {
-  return (
-    <div className={style['button-list']}>
-      {props.contentList.map((v, i) => (
-        <Button key={`button-list--button--${i}`} action={v.action}>
-          {v.child}
-        </Button>
-      ))}
-    </div>
-  )
+const ButtonList: React.FC<Props> = ({ children }) => {
+  return <div className={style['button-list']}>{children}</div>
 }
 
 export default ButtonList

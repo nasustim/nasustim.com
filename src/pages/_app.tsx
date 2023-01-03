95eab11e9dbd3ca024081f84f4554e06efe03840
nasustim <mitsuhibino@gmail.com>
2023-01-03 22:47:21 +0900

    hotfix(ga): broken code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -37,7 +37,9 @@ export default class _App extends App {
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());    
-                gtag('config', '${measurementId}');
+                gtag('config', '${measurementId}', {
+                  page_path: window.location.pathname,
+                });
             `}
             </Script>
             ) : ()

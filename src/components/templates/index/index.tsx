6f631c87195bfe5b9e939786e5e3af078da023f6
nasustim <mitsuhibino@gmail.com>
2023-01-03 21:49:35 +0900

    fix(style): centering main content

--- src/components/templates/index/index.tsx
+++ src/components/templates/index/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import style from './index.module.scss'
 
-import Section from '../../molecules/Layout/Section'
+import Section from '../_utils/Section'
 
 import HeaderProfile from '../../organisms/HeaderProfile'
 
@@ -9,11 +9,9 @@ interface Props {}
 
 const Template: React.FC<Props> = () => (
   <div className={style.template}>
-    <div className={style.template__inner}>
-      <Section>
-        <HeaderProfile />
-      </Section>
-    </div>
+    <Section>
+      <HeaderProfile />
+    </Section>
   </div>
 )
 export default Template

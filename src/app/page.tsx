8fdc75f03fd6ea65dee779164181da817c18370a
nasustim <mitsuhibino@gmail.com>
2023-04-30 18:56:49 +0900

    refactor(app): remove waste element

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,9 @@
 'use client'
 
 import Index from '../components/templates/index/index'
-import Head from 'next/head'
-import { SITE_DOMAIN } from '../constants'
 
 const Page = () => {
-  return (
-    <>
-      <Head>
-        <title>top</title>
-        <link rel='canonical' href={`${SITE_DOMAIN}/`} />
-        <meta name='robots' content='index,follow' />
-      </Head>
-      <Index />
-    </>
-  )
+  return <Index />
 }
 
 export default Page

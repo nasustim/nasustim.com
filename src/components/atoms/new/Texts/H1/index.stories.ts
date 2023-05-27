ab65855dfbc57600169af179ad8bce4e15e62c8a
nasustim <mitsuhibino@gmail.com>
2023-05-27 15:52:32 +0900

    feat(atoms): add H1, H2, H3

---
+++ src/components/atoms/new/Texts/H1/index.stories.ts
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { H1 } from '.'
+
+const defaultProps = {
+  children: 'Heading HogeHoge Hoge',
+}
+
+const meta: Meta<typeof H1> = {
+  title: 'Atoms/Texts/H1',
+  component: H1,
+  tags: ['text'],
+  parameters: {
+    layout: 'fullscreen',
+  },
+}
+
+export default meta
+type Story = StoryObj<typeof H1>
+
+export const Default: Story = {}
+Default.args = defaultProps
---
+++ src/components/atoms/new/Texts/H2/index.stories.ts
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { H2 } from '.'
+
+const defaultProps = {
+  children: 'Heading HogeHoge Hoge',
+}
+
+const meta: Meta<typeof H2> = {
+  title: 'Atoms/Texts/H2',
+  component: H2,
+  tags: ['text'],
+  parameters: {
+    layout: 'fullscreen',
+  },
+}
+
+export default meta
+type Story = StoryObj<typeof H2>
+
+export const Default: Story = {}
+Default.args = defaultProps

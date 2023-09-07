5f967a26dea1cd50e7fabb880ff11f72a650d8b3
nasustim <mitsuhibino@gmail.com>
2023-09-08 01:05:51 +0900

    fix broken img

--- src/app/static/profile_formal.jpg/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import { redirect } from 'next/navigation'
-
-export default function FormalImg() {
-  // 古いドキュメントから`https://nasustim.com/static/profile_formal.webp`を参照していることがあるためリダイレクトする
-  redirect('/static/profile_formal.webp')
-}

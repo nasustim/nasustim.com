c39d37a4d1869a204b455f187940dda115ddfd02
nasustim <mitsuhibino@gmail.com>
2023-10-15 12:23:04 +0900

    chore: ignore lint errs

--- src/components/atoms/ProfileImage/index.tsx
+++ src/components/atoms/ProfileImage/index.tsx
@@ -3,6 +3,7 @@ import styles from './index.module.scss'
 
 type Props = JSX.IntrinsicElements['img']
 
+// eslint-disable-next-line jsx-a11y/alt-text, @next/next/no-img-element
 const ProfileImage: FC<Props> = (props) => <img {...props} className={styles.container} />
 
 export default ProfileImage

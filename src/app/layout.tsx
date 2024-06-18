75cf9f3b4e9e07382f94589de6fbffeeca9388c1
nasustim <mitsuhibino@gmail.com>
2024-06-19 01:16:29 +0900

    feat(SEO): add OpenGraphs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
 import { type ReactNode } from 'react'
-import { SITE_DOMAIN, SITE_TITLE } from '@/constants/values'
+import { SEO, SITE_DOMAIN, SITE_TITLE } from '@/constants/values'
 import { fontClassNames } from '@/fonts'
 import { joinStr } from '@/util/string'
 import { GoogleAnalytics } from '@next/third-parties/google'
@@ -13,7 +13,7 @@ type Props = {
 
 function RootLayout({ children }: Props) {
   return (
-    <html lang='ja' className={joinStr(...fontClassNames)}>
+    <html lang='en' className={joinStr(...fontClassNames)}>
       <head>
         <link rel='icon' href='/favicon.svg' sizes='any' type='image/svg+xml' />
         <link rel='apple-touch-icon' href='/favicon.png' />
@@ -28,10 +28,25 @@ function RootLayout({ children }: Props) {
 
 export default RootLayout
 
+const url = `${SITE_DOMAIN}/`
+
 export const metadata: Metadata = {
   title: `${SITE_TITLE}`,
   alternates: {
-    canonical: `${SITE_DOMAIN}/`,
+    canonical: url,
   },
-  description: 'Mitsuhiro HIBINO is a developer/creator based in Tokyo, Japan.',
+  description: SEO.description,
+  openGraph: {
+    title: SITE_TITLE,
+    description: SEO.description,
+    url: url,
+    type: 'website',
+    locale: 'en_US',
+    siteName: SITE_TITLE,
+    countryName: 'Japan',
+  },
+  twitter: {
+    creatorId: '@nasustim',
+    card: 'summary',
+  },
 }

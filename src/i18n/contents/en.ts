1a0594b738b686cd77c83ffb0d08111140b233ce
nasustim <mitsuhibino@gmail.com>
2025-05-17 17:38:48 +0900

    fix: text

--- src/i18n/contents/en.ts
+++ src/i18n/contents/en.ts
@@ -8,7 +8,7 @@ export const en: I18ncontent = {
   shortDescription: {
     type: "p",
     value: [
-      "A software developer living in Tokyo, Japan.\nI'm currently working on a project supporting a subsidiary's product growth and organizing the engineering process at",
+      "A software developer living in Tokyo, Japan.\nI'm currently working on a project supporting a subsidiary's product growth and organizing the engineering process at ",
       {
         type: "a",
         value: ["MIXI inc"],
@@ -16,7 +16,7 @@ export const en: I18ncontent = {
         areaLabel:
           "Japanese internet company, especially famous as a pioneer of social media in the country",
       },
-      ", as an engineering manager.",
+      ", as an EM.",
     ],
   },
 };

b226e41d4d56e7d3841c0bf659e3b5ae118d5e50
nasustim <mitsuhibino@gmail.com>
2026-05-10 19:33:32 +0900

    Update my description

--- src/i18n/contents/en.ts
+++ src/i18n/contents/en.ts
@@ -8,15 +8,7 @@ export const en: I18ncontent = {
   shortDescription: {
     type: "p",
     value: [
-      "A software developer based in Tokyo.\nI am currently working as a software engineer at ",
-      {
-        type: "a",
-        value: ["MIXI inc."],
-        href: new URL("https://mixi.co.jp"),
-        areaLabel:
-          "Japanese internet company, especially famous as a pioneer of social media in the country",
-      },
-      ", and supporting the growth and improvement of a startup company.",
+      "Software developer based in Tokyo. / Weekend home decorator / Tea lover",
     ],
   },
 };

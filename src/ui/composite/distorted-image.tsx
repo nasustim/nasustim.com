f915f0d57f7ed8748098db10d0d765ff7a62d4b3
nasustim <mitsuhibino@gmail.com>
2026-06-05 22:42:19 +0900

    Organize DistortedImage component

--- src/ui/composite/distorted-image.tsx
+++ src/ui/composite/distorted-image.tsx
@@ -1,17 +1,49 @@
 import { clsx } from "clsx";
 import Image, { type StaticImageData } from "next/image";
-import type { FC } from "react";
-import { useIconAnimation } from "@/style/utilities/icon-animation/useIconAnimation";
+import { type FC, useEffect, useState } from "react";
+
+const defaultRadius = 58;
 
 type Props = {
   src: StaticImageData;
   alt: string;
-  classNames?: string;
+  className?: string;
   size: number;
 };
 
-export const DistortedImage: FC<Props> = ({ src, alt, classNames, size }) => {
-  const { animationClassName } = useIconAnimation();
+function* setRandomRadiusVariables(): Generator<Array<number>, void, unknown> {
+  while (true) {
+    yield Array.from(
+      { length: 4 },
+      () => Math.floor(Math.random() * 14) + defaultRadius, // generate a random number around defaultRadius
+    );
+  }
+}
+
+export const useIconAnimation = () => {
+  const [radius, setRadius] = useState<Array<number>>(
+    Array(4).fill(defaultRadius),
+  );
+
+  useEffect(() => {
+    const animationStepper = setRandomRadiusVariables();
+    animationStepper.next();
+
+    const interval = setInterval(() => {
+      const next = animationStepper.next();
+      if (!next.done) {
+        setRadius(next.value);
+      }
+    }, 5 * 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return radius;
+};
+
+export const DistortedImage: FC<Props> = ({ src, alt, className, size }) => {
+  const radius = useIconAnimation();
 
   return (
     <Image
@@ -19,7 +51,11 @@ export const DistortedImage: FC<Props> = ({ src, alt, classNames, size }) => {
       width={size}
       height={size}
       alt={alt}
-      className={clsx(classNames, animationClassName)}
+      className={clsx(className)}
+      style={{
+        borderRadius: radius.map((r) => `${r}px`).join(" "),
+        transition: "border-radius 5s ease-in-out",
+      }}
     />
   );
 };
